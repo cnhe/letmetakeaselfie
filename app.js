fe12c81bc16a31788bb14317cdb6081c070d92f3
cnhe <christinewuz@gmail.com>
2014-04-18 06:27:16 -0700

    i have no idea what i'm doing

--- app.js
+++ app.js
@@ -12,6 +12,38 @@ var index = require('./routes/index');
 //var mongoose = require('mongoose');
 //mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
 
+ // get FB authorization url
+    var authUrl = graph.getOauthUrl({
+        "client_id":     dotenv.FB_APPID
+      , "redirect_uri":  dotenv.ROOT_URL
+    });
+
+    // shows dialog
+    res.redirect(authUrl);
+
+    // after user click, auth `code` will be set
+    // we'll send that and get the access token
+    graph.authorize({
+        "client_id":      dotenv.FB_APPID
+      , "redirect_uri":   dotenv.ROOT_URL
+      , "client_secret":  dotenv.FB_APPSECRET
+      , "code":           req.query.code
+    }, function (err, facebookRes) {
+      res.redirect('/loggedIn');
+    });
+
+var options = {
+	timeout:  3000
+	, pool:     { maxSockets:  Infinity }
+	, headers:  { connection:  "keep-alive" }
+};
+
+graph
+  .setOptions(options)
+  .get("zuck", function(err, res) {
+    console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
+  });
+
 //Configures the Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
--- auth.js
+++ auth.js
@@ -2,16 +2,22 @@
 var dotenv = require('dotenv');
 dotenv.load();
 
-
-/**
-* Add your authentication apis here with example like the bottom
-*/
-/**
-//add instagram api setup
+/*//add instagram api setup
 var ig = require('instagram-node-lib');
 ig.set('client_id', process.env.instagram_client_id);
 ig.set('client_secret', process.env.instagram_client_secret);
 
 //export ig as a parameter to be used by other methods that require it.
-exports.ig = ig;
-**/
+exports.ig = ig;*/
+
+//add fbgraph
+var graph = require('fbgraph');
+graph.set('client_id', process.env.FB_APPID);
+graph.set('client_secret', process.env.FB_APPSECRET);
+
+var twit = require('twit');
+twit.set('client_id', process.env.TWITTER_APPID);
+twit.set('client_secret', process.env.TWITTER_APPSECRET);
+
+exports.graph = graph;
+exports.twit = twit;

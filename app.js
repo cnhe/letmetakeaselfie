cdcb1214bf03b5ec31b69670e257aad9226294ae
cnhe <christinewuz@gmail.com>
2014-04-18 23:33:19 -0700

    :(

--- app.js
+++ app.js
@@ -10,46 +10,12 @@ var twit = require('twit');
 var dotenv = require('dotenv');
 dotenv.load();
 
-app.get('/auth/facebook', function(req, res) {
-//app.get('./auth.js', function(req, res) {
-
-  // we don't have a code yet
-  // so we'll redirect to the oauth dialog
-  if (!req.query.code) {
-    var authUrl = graph.getOauthUrl({
-        "client_id":     process.env.FB_APPID
-      , "redirect_uri":  process.env.FB_ROOTURL
-      , "scope":         process.env.FB_SCOPE
-    });
-
-    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
-      res.redirect(authUrl);
-    } else {  //req.query.error == 'access_denied'
-      res.send('access denied');
-    }
-     return;
-  }
-
-  // code is set
-  // we'll send that and get the access token
-  graph.authorize({
-      "client_id":      process.env.FB_APPID
-    , "redirect_uri":   process.env.FB_ROOTURL
-    , "client_secret":  process.env.FB_APPSECRET
-    , "code":           req.query.code
-  }, function (err, facebookRes) {
-    res.redirect('/UserHasLoggedIn');
-  });
-
-  // user gets sent here after being authorized
-  app.get('/UserHasLoggedIn', function(req, res) {
-  res.render("index", { title: "Logged In" });
-  });
+var t = new twit({
+    consumer_key:         process.env.TWIT_CONKEY
+  , consumer_secret:      process.env.TWIT_CONSECRET
+  , access_token:         process.env.TWIT_ACCESSTOKEN
+  , access_token_secret:  process.env.TWIT_ACCESSSECRET
 });
-
-//route files to load
-var index = require('./routes/index');
-
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
@@ -57,6 +23,73 @@ app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
+
+//route files to load
+var index = require('./routes/index');
+//var loggedin = require('./routes/loggedin');
+
+exports.graph = graph;
+exports.twit = twit;
+
+t.get('search/tweets', { q: '#selfie', count: 10 }, function(err, reply) {
+  //console.log(reply);
+})
+
+app.get('/auth/facebook', function(req, res) {
+	// we don't have a code yet
+	// so we'll redirect to the oauth dialog
+	if (!req.query.code) {
+		var authUrl = graph.getOauthUrl({
+			"client_id":     process.env.FB_APPID
+			, "redirect_uri":  process.env.FB_ROOTURL
+			, "scope":         process.env.FB_SCOPE
+		});
+
+		if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+			res.redirect(authUrl);
+		} 
+		else {  //req.query.error == 'access_denied'
+			res.send('access denied');
+		}
+
+		console.log("return? also what is code");
+		console.log(req.query.code);
+
+		return;
+	}
+
+	console.log(req.query.code);
+	// code is set
+	// we'll send that and get the access token
+	graph.authorize({
+		"client_id":      process.env.FB_APPID
+		, "redirect_uri":   process.env.FB_ROOTURL
+		, "client_secret":  process.env.FB_APPSECRET
+		, "code":           req.query.code
+	}, function (err, facebookRes) 
+	{
+		console.log("login success?");
+		res.redirect('/loggedin');
+	});
+});
+
+console.log("access token");
+console.log(graph.getAccessToken());
+
+app.get('/test', function(req,res) { 
+	graph.get('/me', function(err,response) { console.log(response); res.render('index')}); });
+
+app.get('/loggedin', function(req, res){
+	res.render("index", {title: "Logged in"})
+});
+
+  graph.get('/me/likes', function(req, res){
+	console.log(res);
+  });
+
+  graph.get("zuck", function(err, res){
+  		console.log(res);
+  	});
 
 //routes
 app.get('/', index.view);
